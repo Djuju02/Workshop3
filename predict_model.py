34bb4719c98d1b3cd02589ea371ec954d26ded98
Djuju02 <91671095+Djuju02@users.noreply.github.com>
2024-02-05 17:44:43 +0100

    Update predict_model.py
    Préparation du fichier pour éviter les conflits de forks

---
+++ predict_model.py
@@ -0,0 +1,14 @@
+#------------Part of Mathias------------
+def Mathias():
+
+
+#------------Part of Manon------------
+def Manon():
+
+
+#------------Part of Tiphaine------------
+def Tiphaine():
+
+
+#------------Part of Julien------------
+def Julien():
