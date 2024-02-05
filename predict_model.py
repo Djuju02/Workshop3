95acbc7257c7a7203be579f61a76a00825a9ef28
manon gardin <manon.gardin@edu.devinci.fr>
2024-02-05 17:47:01 +0100

    Data importation

--- predict_model.py
+++ predict_model.py
@@ -1,5 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import statistics
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+
+#Data importation
+df = pd.read_csv('archive/NY-House-Dataset.csv', sep=',')
+
 #------------Part of Mathias------------
-def Mathias():
+def Matias():
 
 
 #------------Part of Manon------------
