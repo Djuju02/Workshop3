2bdb3fbd9072906799abf2790bd43cf3c74c26dd
manon gardin <manon.gardin@edu.devinci.fr>
2024-02-05 18:18:44 +0100

    English transition

--- predict_model.py
+++ predict_model.py
@@ -12,29 +12,107 @@ from sklearn.metrics import mean_squared_error
 
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 
+#--------------- Cleaning data ---------------
+def cleaning_data(df):
+    # Delete duplicates 
+    print(df.duplicated().any())
+    df = df.drop_duplicates()
+    print(df.duplicated().any())
+
+    # Visualise Outliers
+    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(20, 5))
+
+    df[['PRICE']].boxplot(ax=axes[0])
+    axes[0].set_title('PRICE')
+
+    df[['BEDS']].boxplot(ax=axes[1])
+    axes[1].set_title('BEDS')
+
+    df[['BATH']].boxplot(ax=axes[2])
+    axes[2].set_title('BATH')
+
+    df[['PROPERTYSQFT']].boxplot(ax=axes[3])
+    axes[3].set_title('PROPERTYSQFT')
+
+    plt.show()
+
+    # Manage Outliers
+    #Outliers for PRICE
+    q1 = df['PRICE'].quantile(0.25)
+    q3 = df['PRICE'].quantile(0.75)
+
+    IQR = q3 - q1
+
+    max_limit = q3 + (1.5 * IQR)
+    min_limit = q1 - (1.5 * IQR) 
+
+    df.loc[df['PRICE'] > max_limit, 'PRICE'] = max_limit
+    df.loc[df['PRICE'] < min_limit, 'PRICE'] = min_limit
+
+    #Outliers for BEDS
+    q1_beds = df['BEDS'].quantile(0.25)
+    q3_beds = df['BEDS'].quantile(0.75)
+
+    IQR_beds = q3_beds - q1_beds
+
+    max_limit_beds = q3_beds + (1.5 * IQR_beds)
+    min_limit_beds = q1_beds - (1.5 * IQR_beds)
+
+    df.loc[df['BEDS'] > max_limit_beds, 'BEDS'] = max_limit_beds
+    df.loc[df['BEDS'] < min_limit_beds, 'BEDS'] = min_limit_beds
+
+    #Outliers for PROPERTYSQFT
+    q1_propertysqft = df['PROPERTYSQFT'].quantile(0.25)
+    q3_propertysqft = df['PROPERTYSQFT'].quantile(0.75)
+
+    IQR_propertysqft = q3_propertysqft - q1_propertysqft
+
+    max_limit_propertysqft = q3_propertysqft + (1.5 * IQR_propertysqft)
+    min_limit_propertysqft = q1_propertysqft - (1.5 * IQR_propertysqft)
+
+    df.loc[df['PROPERTYSQFT'] > max_limit_propertysqft, 'PROPERTYSQFT'] = max_limit_propertysqft
+    df.loc[df['PROPERTYSQFT'] < min_limit_propertysqft, 'PROPERTYSQFT'] = min_limit_propertysqft
+
+    #Outliers for BATH
+    q1_bath = df['BATH'].quantile(0.25)
+    q3_bath = df['BATH'].quantile(0.75)
+
+    IQR_bath = q3_bath - q1_bath
+
+    max_limit_bath = q3_bath + (1.5 * IQR_bath)
+    min_limit_bath = q1_bath - (1.5 * IQR_bath)
+
+    df.loc[df['BATH'] > max_limit_bath, 'BATH'] = max_limit_bath
+    df.loc[df['BATH'] < min_limit_bath, 'BATH'] = min_limit_bath
+
 def Matias():
-    # Votre code pour Matias ici
+    # Matias' code here
     print('Fonction de Matias exécutée')
     pass
 
 def Manon():
-    # Votre code pour Manon ici
+    # Manon's code here
     print('Fonction de Manon exécutée')
     pass
 
 def Tiphaine():
-    # Votre code pour Tiphaine ici
+    # Tiphaine's code here
     print('Fonction de Tiphaine exécutée')
     pass
 
 def Julien():
-    # Votre code pour Julien ici
+    # Julien's code here
     print('Fonction de Julien exécutée')
     pass
 
+def Minsoo():
+    #Minsoo's code here
+    print("Minsoo's function is working")
+    pass
+
 def main():
-    # Demander à l'utilisateur quelle fonction exécuter
-    choix = input("Quelle fonction voulez-vous exécuter ? (Matias, Manon, Tiphaine, Julien): ")
+    # Ask the user which function to execute
+    choix = input("Which function do you want to execute ? (Matias, Manon, Tiphaine, Julien, Minsoo): ")
     
     if choix == 'Matias':
         Matias()
@@ -44,15 +122,18 @@ def main():
         Tiphaine()
     elif choix == 'Julien':
         Julien()
+    elif choix == 'Minsoo':
+        Minsoo()
     else:
-        print("Choix non reconnu. Veuillez entrer un nom valide.")
+        print("Choice not recognized. Please enter a valid name.")
 
 if __name__ == "__main__":
-    # Importation des données dans le bloc principal pour éviter l'exécution lors des importations
     try:
         df = pd.read_csv('archive/NY-House-Dataset.csv', sep=',')
+        cleaning_data(df)
         print(df)
     except FileNotFoundError:
-        print("Fichier non trouvé. Veuillez vérifier le chemin.")
+        print("File not found. Please check the path.")
     
     main()
+
