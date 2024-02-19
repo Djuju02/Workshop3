ccb238d749f1be2a53e9fb7db8f103130f1e5203
manon gardin <manon.gardin@edu.devinci.fr>
2024-02-19 01:06:29 +0100

    Update SQL add cart

--- PostgreSQL/InsertPark.sql
+++ PostgreSQL/InsertPark.sql
@@ -5,23 +5,30 @@ INSERT INTO products (name, description, price, stockquantity) VALUES
 ('Chaussures de sport', 'Chaussures de course légères', 49.99, 80);
 
 
-INSERT INTO customers (FirstName, LastName, Email, Address, City, PostalCode, Country) VALUES
+INSERT INTO customers (firstname, lastname, email, address, city, postalcode, country) VALUES
 ('Jean', 'Dupont', 'jean.dupont@example.com', '123 rue de la République', 'Paris', '75001', 'France'),
 ('Marie', 'Dubois', 'marie.dubois@example.com', '456 avenue des Champs-Élysées', 'Paris', '75008', 'France'),
 ('John', 'Smith', 'john.smith@example.com', '789 Main Street', 'New York', '10001', 'USA'),
 ('Emma', 'Johnson', 'emma.johnson@example.com', '456 Elm Street', 'Los Angeles', '90001', 'USA');
 
 
-INSERT INTO orders (CustomerID, TotalAmount) VALUES
+INSERT INTO orders (customerid, totalamount) VALUES
 (1, 79.97),
 (2, 109.97),
 (3, 99.98),
 (4, 69.98);
 
 
-INSERT INTO orderdetails (OrderID, ProductID, Quantity, Price) VALUES
+INSERT INTO orderdetails (orderid, productid, quantity, price) VALUES
 (1, 1, 2, 39.98),
 (2, 2, 3, 89.97),
 (3, 3, 1, 39.99),
 (4, 4, 1, 49.99),
 (4, 1, 1, 19.99);
+
+INSERT INTO cart (customerid, productid, quantity, price) VALUES
+(1, 1, 2, 39.98),
+(2, 2, 3, 89.97),
+(3, 3, 1, 39.99),
+(4, 4, 1, 49.99),
+(1, 2, 1, 29.99);
