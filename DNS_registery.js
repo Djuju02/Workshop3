59fd75c68ee59b4306cc495c2399873e22b78e53
manon gardin <manon.gardin@edu.devinci.fr>
2024-02-19 01:52:24 +0100

    Light edit of DNS_registery.js

--- DNS_registery.js
+++ DNS_registery.js
@@ -1,17 +1,13 @@
 const express = require('express');
-import { LearningPackage, LearningFact } from "./DB_connection";
 
 const app = express();
-const PORT= 3000;
-
-app.use(express.json())
 
 app.get('/getServer', (req, res) => {
-    const serverURL = req.protocol + '://' + req.get('host');
+    const serverURL = req.get('host');
     res.json({ code: 200, server: serverURL });
 });
 
-//const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
